9b08aaa7c347174fc0176081796693e795ac21c6
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-06-24 15:20:20 +0300

    updates for responsive

--- src/components/From.js
+++ src/components/From.js
@@ -27,25 +27,27 @@ export default function Form({ onAddItems }) {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(eventChange) =>
-          setQuantity(Number(eventChange.target.value))
-        }
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(eventChange) => setDescription(eventChange.target.value)}
-      />
-      <button>Add</button>
+      <div className="fieldsBlock">
+        <select
+          value={quantity}
+          onChange={(eventChange) =>
+            setQuantity(Number(eventChange.target.value))
+          }
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(eventChange) => setDescription(eventChange.target.value)}
+        />
+        <button>Add</button>
+      </div>
     </form>
   );
 }
